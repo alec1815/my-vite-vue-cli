74277d67fd036a495889f1539c3e8ea787da8f7e
wzhaofei <505139165@qq.com>
2022-03-01 17:53:11 +0800

    imitate vite HMR

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
+export function render(){
+  document.querySelector("#app")!.innerHTML = "main"
+}
+
+render()
+
+if(import.meta.hot){
+  import.meta.hot.accept((newModule)=>{
+    console.log("render")
+    newModule.render()
+  })
+}
+
 console.log("page main")
